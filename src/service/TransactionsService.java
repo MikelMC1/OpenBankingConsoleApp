bbf430ef003f9025a5d25c24d34bd152b9543dbb
MikelMC1 <mskenderi22@epoka.edu.al>
2025-09-12 11:58:02 +0200

    makeTransfer between 2 users

--- src/service/TransactionsService.java
+++ src/service/TransactionsService.java
@@ -1,9 +1,33 @@
 package service;
 
+import excpetion.AuthenticationException;
+import excpetion.InsufficientBalanceException;
+
 public class TransactionsService {
 
 
-    public void getTransactions() {
+
+
+    private BalanceService balanceService;
+
+    public TransactionsService(BalanceService balanceService) {
+        this.balanceService = balanceService;
     }
 
+    public void makeTransfer(long fromUserId, long toUserId, double amount)
+            throws AuthenticationException, InsufficientBalanceException {
+
+        balanceService.withdrawMoney(fromUserId, amount);
+
+        balanceService.depositMoney(toUserId, amount);
+
+        System.out.println("Transferred " + amount + " from User " + fromUserId + " to User " + toUserId);
+    }
+
+    //TODO
+    public void getTransactions() {
+
+
+
+    }
 }
